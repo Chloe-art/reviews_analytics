3e61aa87a6cde2dec87330afd041b3110af8cdfd
Chloe Wang <Chocolatepei@gmail.com>
2020-01-11 10:27:40 +0800

    review_analytics1

--- review_analytics.py
+++ review_analytics.py
@@ -1,11 +1,16 @@
 #讀取檔案練習
 
 data = []
+count = 0
 with open('reviews.txt', 'r') as f:
 	for message in f:
 		data.append(message)
+		count += 1 #count = count + 1
+		if count % 10000 == 0:
+			print(len(data))
 print(len(data))
 
-print(data[123456])
+print(data[9])
 print('----------------------')
-print(data[4])
+print(data[5])
+
