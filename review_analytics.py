21a11fd3501c2e4b033947234481a725bff1d9bd
Chloe Wang <Chocolatepei@gmail.com>
2020-01-12 16:52:48 +0800

    cal_2

--- review_analytics.py
+++ review_analytics.py
@@ -6,7 +6,7 @@ with open('reviews.txt', 'r') as f:
 	for message in f:
 		data.append(message)
 		count += 1 #count = count + 1
-		if count % 10000 == 0:
+		if count % 100000 == 0:
 			print(len(data))
 print(len(data))
 
@@ -27,3 +27,10 @@ for d in data:
 print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
 
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言提到good')
+print(good[12345])
